2db51c32927c3168907898e4cf62a14d4d9734b4
Knopp Konstantin <k.knopp@das-unikat.com>
2018-03-18 18:02:08 +0100

    height adjustment

--- datatables.js
+++ datatables.js
@@ -44,15 +44,60 @@
             body.find('td').addClass('ux-datatable__body-column');
         };
 
+        var getScrollbarWidth = function () {
+            var outer = document.createElement("div");
+            outer.style.visibility = "hidden";
+            outer.style.width = "100px";
+            outer.style.msOverflowStyle = "scrollbar"; // needed for WinJS apps
+
+            document.body.appendChild(outer);
+
+            var widthNoScroll = outer.offsetWidth;
+            // force scrollbars
+            outer.style.overflow = "scroll";
+
+            // add innerdiv
+            var inner = document.createElement("div");
+            inner.style.width = "100%";
+            outer.appendChild(inner);
+
+            var widthWithScroll = inner.offsetWidth;
+
+            // remove divs
+            outer.parentNode.removeChild(outer);
+
+            return widthNoScroll - widthWithScroll;
+        };
+
+        var getBodyHeight = function () {
+            var height = 0;
+
+            body.find('tr').each(function() {
+                height += ($(this).height() + 1);
+            });
+
+            return height - 1;
+        };
+
+
+        var adjustForScrollbar = function () {
+            if(body[0].scrollHeight > 400) {
+                header.css('padding-right', getScrollbarWidth());
+            } else {
+                body.css('height', getBodyHeight());
+            }
+        };
+
         var init = function () {
             buildStructure();
+            adjustForScrollbar();
 
             body.on('scroll', function() {
                 if(body.scrollTop() > 0) {
-                    header.addClass('sticky');
+                    header.addClass('h-border-bottom');
                 }
                 else {
-                    header.removeClass('sticky');
+                    header.removeClass('h-border-bottom');
                 }
             });
         };
