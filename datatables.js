3712fe70d049df42b2748e7ee4ab4ed6dfceaae1
Konstantin Knopp <k.knopp@das-unikat.com>
2018-03-19 16:01:57 +0100

    style update

--- datatables.js
+++ datatables.js
@@ -1,12 +1,36 @@
-"use strict";
+/*! UX DataTables 0.1.0
+ *  ©2018-2018 Unikat GmbH
+ */
+
+/**
+ * @summary DataTables
+ * @description
+ * @version 0.1.0
+ * @file datatables.js
+ * @author Unikat GmbH
+ * @contact www.das-unikat.com
+ * @copyright Copyright 2018-2018 Unikat GmbH
+ */
 
 (function($) {
+	"use strict";
+    
     $.fn.dataTable = function(options) {
-        var settings = $.extend({}, $.fn.dataTable.defaults, options);
+	    /**
+         * Plugin settings
+	     */
+	    var settings = $.extend({}, $.fn.dataTable.defaults, options);
         var $this = $(this);
 
         $.fn.dataTable.defaults = {};
-
+	
+	    /**
+         * Get the plugin options
+         *
+	     * @param name
+	     * @param value
+	     * @returns {*}
+	     */
         $.fn.dataTable.option = function (name, value) {
             name = name || false;
             value = value || false;
@@ -29,10 +53,18 @@
                 }
             }
         };
-
-        var header, body;
-
-        var buildStructure = function () {
+	
+	    /**
+         * Global header and body selectors
+	     */
+	    var header, body;
+	
+	    /**
+         * Build the table structure for the plugin
+         *
+	     * @private
+	     */
+	    var _fbBuildStructure = function () {
             $this.addClass('ux-datatable');
 
             header = $this.find('thead').addClass('ux-datatable__header');
@@ -43,8 +75,14 @@
             body.find('tr').addClass('ux-datatable__body-row');
             body.find('td').addClass('ux-datatable__body-column');
         };
-
-        var getScrollbarWidth = function () {
+	
+	    /**
+         * Get the browser scrollbar width
+         *
+	     * @returns {number}
+	     * @private
+	     */
+        var _fnGetScrollbarWidth = function () {
             var outer = document.createElement("div");
             outer.style.visibility = "hidden";
             outer.style.width = "100px";
@@ -53,23 +91,26 @@
             document.body.appendChild(outer);
 
             var widthNoScroll = outer.offsetWidth;
-            // force scrollbars
             outer.style.overflow = "scroll";
-
-            // add innerdiv
+            
             var inner = document.createElement("div");
             inner.style.width = "100%";
             outer.appendChild(inner);
 
             var widthWithScroll = inner.offsetWidth;
-
-            // remove divs
+            
             outer.parentNode.removeChild(outer);
 
             return widthNoScroll - widthWithScroll;
         };
-
-        var getBodyHeight = function () {
+	
+	    /**
+         * Get the full height of the inner table body
+         *
+	     * @returns {number}
+	     * @private
+	     */
+        var _fnGetBodyHeight = function () {
             var height = 0;
 
             body.find('tr').each(function() {
@@ -78,19 +119,33 @@
 
             return height - 1;
         };
-
-
-        var adjustForScrollbar = function () {
-            if(body[0].scrollHeight > 400) {
-                header.css('padding-right', getScrollbarWidth());
-            } else {
-                body.css('height', getBodyHeight());
+	
+	    /**
+         * Adjust the table when there is or is not a browser scrollbar
+         *
+	     * @private
+	     */
+	    var _fnAdjustForScrollbar = function () {
+		    /**
+             * If body larger than given height, adjust header for scrollbar
+		     */
+		    if(body[0].scrollHeight > 400) {
+                header.css('padding-right', _fnGetScrollbarWidth());
+            }
+		    /**
+             * If body smaller than given height, adjust body height to fit content
+		     */
+		    else {
+                body.css('height', _fnGetBodyHeight());
             }
         };
-
-        var init = function () {
-            buildStructure();
-            adjustForScrollbar();
+	
+	    /**
+         * Initialize the plugin
+	     */
+	    var init = function () {
+            _fbBuildStructure();
+            _fnAdjustForScrollbar();
 
             body.on('scroll', function() {
                 if(body.scrollTop() > 0) {
